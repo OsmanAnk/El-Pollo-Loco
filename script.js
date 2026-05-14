33dddbb61a602bf0a150345e087faa5c035c5a27
OsmanAnk <osman.annak@hotmail.de>
2026-05-14 21:51:15 +0200

    imprint and keybind modal added

--- script.js
+++ script.js
@@ -3,3 +3,52 @@ function startGame() {
     startScreen.style.display = "none";
     init();
 }
+
+function eventBubbling(event) {
+    event.stopPropagation();
+}
+
+function toggleSound() {
+    const sound = document.getElementById("sound-btn");
+    if (sound.src.includes("lautstarke.png")) {
+        sound.src = "assets/icons/klang.png";
+    }
+    else {
+        sound.src = "assets/icons/lautstarke.png";
+    }
+}
+
+function toggleFullscreen() {
+    const canvas = document.getElementById("canvas");
+    if (!document.fullscreenElement) {
+        canvas.innerWidth = window.innerWidth;
+        canvas.innerHeight = window.innerHeight;
+    } else {
+        document.exitFullscreen();
+        resizeCanvas(canvas);
+    }
+}
+
+function resizeCanvas(canvas) {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    World.draw();
+}
+
+function showControls() {
+    const keybindsRef = document.getElementById("keyboard-controls-screen");
+    keybindsRef.classList.remove("d_none");
+}
+
+function closeModal() {
+    const keybindsRef = document.getElementById("keyboard-controls-screen");
+    const imprintRef = document.getElementById("imprint-screen");
+    keybindsRef.classList.add("d_none");
+    imprintRef.classList.add("d_none");
+}
+
+function showImprint() {
+    const imprintRef = document.getElementById("imprint-screen");
+    imprintRef.classList.remove("d_none");
+}
